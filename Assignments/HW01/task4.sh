b15822d644ec6e499fe212fc2a18a70274823efd
bawi <salai.ukvanbawi@gmail.com>
2026-09-11 23:49:07 -0500

    Initial HW01

---
+++ Assignments/HW01/task4.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#SBATCH --cpus-per-task=2
+#SBATCH --job-name=FirstSlurm
+#SBATCH --output=FirstSlurm.out
+#SBATCH --error=FirstSlurm.err
+
+hostname
---
+++ Assignments/HW01/task6.cpp
@@ -0,0 +1,13 @@
+#include <cstdio>
+#include <cstdlib>
+#include <iostream>
+
+int main(int argc, char* argv[]) {
+    // TODO: read N from argv
+
+    // TODO: printf from 0 to N
+
+    // TODO: std::cout from N to 0
+
+    return 0;
+}
